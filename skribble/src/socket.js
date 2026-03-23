1691d1e3994c49b3cbf9489b9b1e322d198fdcf8
muskan931pra <muskan931pra@gmail.com>
2026-03-23 11:04:58 +0530

    update socket url

--- skribble/src/socket.js
+++ skribble/src/socket.js
@@ -1,3 +1,7 @@
 import {io} from "socket.io-client"
 
-export const socket = io("http://localhost:3000")
+const URL = import.meta.env.MODE === "production"
+    ? "https://skribble-server-9v21.onrender.com"  // ← paste your render URL here
+    : "http://localhost:3000";
+
+export const socket = io(URL);
