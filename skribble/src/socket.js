dd6f4a1dae469a88816cd59934df260af1ee881e
muskan931pra <muskan931pra@gmail.com>
2026-03-24 11:36:02 +0530

    fix mouse support

--- skribble/src/socket.js
+++ skribble/src/socket.js
@@ -3,7 +3,7 @@ import {io} from "socket.io-client"
 // export const socket = io("http://localhost:3000")
 
 const URL = import.meta.env.MODE === "production"
-    ? "https://skribble-server-9v21.onrender.com"  // ← paste your render URL here
+    ? "https://skribble-server-9v21.onrender.com"  
     : "http://localhost:3000";
 
 export const socket = io(URL);
